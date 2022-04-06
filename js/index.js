43145789ac0707b723ba6616dc4fbcde745ada9c
pachocki <101109218+pachocki@users.noreply.github.com>
2022-04-06 11:29:39 +0200

    new section

--- js/index.js
+++ js/index.js
@@ -52,6 +52,31 @@ var image = document.getElementsByClassName('ifour');
           orientation: 'right'
     });
 
+//Swiper JS 
+var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      },
+    });
+
 
 
 
