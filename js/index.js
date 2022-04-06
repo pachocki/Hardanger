15ad3554e69ac588faae9a42b05c6580a35347e1
pachocki <101109218+pachocki@users.noreply.github.com>
2022-04-07 01:32:01 +0200

    cards

--- js/index.js
+++ js/index.js
@@ -57,21 +57,17 @@ var swiper = new Swiper(".mySwiper", {
       slidesPerView: 1,
       spaceBetween: 10,
       grabCursor: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
       breakpoints: {
         640: {
           slidesPerView: 2,
           spaceBetween: 20,
         },
         768: {
-          slidesPerView: 4,
+          slidesPerView: 3,
           spaceBetween: 40,
         },
         1024: {
-          slidesPerView: 5,
+          slidesPerView: 4,
           spaceBetween: 20,
         },
       },
